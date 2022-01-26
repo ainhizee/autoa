b283ed2bbd6283d22a9b6c31fbaf708955d99a2d
ainhizee <93262250+ainhizee@users.noreply.github.com>
2022-01-26 14:26:58 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (0 == 1) {
+    	
+    }
+    if (0 == 2) {
+    	
+    }
+    if (0 == 3) {
+    	
+    }
+})
 function ezkerreko_intermitentea () {
     for (let index = 0; index < 4; index++) {
         basic.showLeds(`
@@ -57,12 +68,12 @@ function eskuineko_intermitentea () {
 radio.setGroup(3)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
-    	
+        radio.sendNumber(1)
     }
-    if (input.buttonIsPressed(Button.A)) {
-    	
+    if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
     }
-    if (input.buttonIsPressed(Button.A)) {
-    	
+    if (input.logoIsPressed()) {
+        radio.sendNumber(3)
     }
 })
